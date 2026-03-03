a3afcf19ad4ac0f83d362c8a36bb36a6963b6165
Janrey <janreyminadev@gmail.com>
2026-03-03 16:54:11 +0800

    office

--- android/app/src/main/kotlin/com/example/spacall_therapist_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.spacall_therapist_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/ph/spacall/therapist/MainActivity.kt
@@ -0,0 +1,5 @@
+package ph.spacall.therapist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
